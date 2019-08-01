4fdea25553c563c0797d55e9f3f04ea8f17ef3fd
N-bred <thehardyboiz1@gmail.com>
2019-08-01 11:35:58 -0500

    Deploy

---
+++ precache-manifest.7ebb137ff44211ad42433bf1c1fead0a.js
@@ -0,0 +1,34 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "c30718423302473f5c33f1440fadd465",
+    "url": "/pomodoro-clock/index.html"
+  },
+  {
+    "revision": "40059c90d6ea7f8d1e1b",
+    "url": "/pomodoro-clock/static/css/main.a4140cad.chunk.css"
+  },
+  {
+    "revision": "f6ba9d04efe7d4763d09",
+    "url": "/pomodoro-clock/static/js/2.d2fd1c92.chunk.js"
+  },
+  {
+    "revision": "40059c90d6ea7f8d1e1b",
+    "url": "/pomodoro-clock/static/js/main.6ccc25f0.chunk.js"
+  },
+  {
+    "revision": "aa817215ef5e490bad59",
+    "url": "/pomodoro-clock/static/js/runtime~main.647dc629.js"
+  },
+  {
+    "revision": "00751e6fc05ba2152ea559e35646d613",
+    "url": "/pomodoro-clock/static/media/alarm1.00751e6f.mp3"
+  },
+  {
+    "revision": "1a566908e0f7d5f38aa810ca105e15fa",
+    "url": "/pomodoro-clock/static/media/minus.1a566908.svg"
+  },
+  {
+    "revision": "f247d9f9ae1dda3148aff3ffc2b676a2",
+    "url": "/pomodoro-clock/static/media/plus.f247d9f9.svg"
+  }
+]);
--- src/audioArray.js
+++
@@ -1,3 +0,0 @@
-import alarm1 from './audio/alarm1.mp3';
-
-export default [alarm1];
